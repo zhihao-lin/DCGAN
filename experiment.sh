9934ffdec01d2caa51495f812ccdbb5a4c99cd23
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-02-21 16:28:16 +0000

    add

--- experiment.sh
+++ experiment.sh
@@ -1,4 +1,4 @@
 # python3 main.py train g_01 d_01 -id 001
 # python3 main.py train g_02 d_02 -id 002
 # python3 main.py train g_02 d_02 -id 003
-python3 main.py train g_tutorial d_tutorial -id tutorial -info "folow the tutorial"
+python3 main.py train g_tutorial d_tutorial -id reproduce 
