1b581a43181262ae96c37e7dae122254e59b741a
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-02-28 10:40:38 +0000

    result of exp3

--- experiment.sh
+++ experiment.sh
@@ -3,4 +3,5 @@
 # python3 main.py train g_02 d_02 -id 003
 # python3 main.py train g_tutorial d_tutorial -id reproduce 
 # python3 main.py train g_tutorial d_tutorial -id exp1 -info "without initialization"
-python3 main.py train g_tutorial d_tutorial -id exp2 -info "change learning rate" -lr 1e-4
+# python3 main.py train g_tutorial d_tutorial -id exp2 -info "change learning rate" -lr 1e-4
+python3 main.py train g_tutorial d_tutorial -id exp3 -info "change learning rate to 1e-2" -lr 1e-2
