a771f79310a71c6e571a3df723a83177507d2de0
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-02-28 12:27:13 +0000

    test beta

--- model_manager.py
+++ model_manager.py
@@ -16,8 +16,9 @@ class Manaeger():
             model_G.load_state_dict(torch.load(load_name + '_G.pkl'))
             model_D.load_state_dict(torch.load(load_name + '_D.pkl'))
         else:
-            model_G.apply(initialize_weights)
-            model_D.apply(initialize_weights)
+            pass
+            # model_G.apply(initialize_weights)
+            # model_D.apply(initialize_weights)
         
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model_G = model_G.to(self.device)
