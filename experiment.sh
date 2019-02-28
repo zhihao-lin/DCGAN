218029ff6c779c72cdec1db8ce1889c58783c1f8
James Lin <b04901069@ntu.edu.tw>
2019-02-28 16:43:51 +0800

    new exp1

--- experiment.sh
+++ experiment.sh
@@ -1,4 +1,5 @@
 # python3 main.py train g_01 d_01 -id 001
 # python3 main.py train g_02 d_02 -id 002
 # python3 main.py train g_02 d_02 -id 003
-python3 main.py train g_tutorial d_tutorial -id reproduce 
+# python3 main.py train g_tutorial d_tutorial -id reproduce 
+python3 main.py train g_tutorial d_tutorial -id exp1 -info "without initialization"
