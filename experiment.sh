83202cc0b0d08ed51a89985e37d8876af367e3e3
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-02-28 11:36:43 +0000

    add exp4

--- experiment.sh
+++ experiment.sh
@@ -4,4 +4,5 @@
 # python3 main.py train g_tutorial d_tutorial -id reproduce 
 # python3 main.py train g_tutorial d_tutorial -id exp1 -info "without initialization"
 # python3 main.py train g_tutorial d_tutorial -id exp2 -info "change learning rate" -lr 1e-4
-python3 main.py train g_tutorial d_tutorial -id exp3 -info "change learning rate to 1e-2" -lr 1e-2
+# python3 main.py train g_tutorial d_tutorial -id exp3 -info "change learning rate to 1e-2" -lr 1e-2
+python3 main.py train g_tutorial d_tutorial -id exp4 -info "use default beta value for Adam" -beta 0.9
